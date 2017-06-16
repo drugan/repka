f721cd46c1a16ae54c96cea954319950a50603c8
drugan <drugan@1578644.no-reply.drupal.org>
2017-06-16 19:22:24 +0300

    Added files to reproduce the issue

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Hello GitHub!!!
+Read more: https://github.com/eslint/eslint/issues/8738
---
+++ parent_project/child_project/folder1/child.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+    var a = 1;
+    var b = [1, 2, 3 ];
+
+    if (a === -0) {
+        // doSomething()...
+        a = b;
+    }
+})();
